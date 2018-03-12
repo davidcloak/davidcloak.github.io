f4d619ee73ce8437750db2d5090c1670261a944d
davidcloak <35105111+davidcloak@users.noreply.github.com>
2018-03-12 17:03:11 -0400

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,61 +1,192 @@
-?php 
-  /*
-  * Warning! Read and use at your own risk!
-  *
-  * This tiny proxy script is completely transparent and it passes 
-  * all requests and headers without any checking of any kind.
-  * The same happens with JSON data. They are simply forwarded.
-  *
-  * This is just an easy and convenient solution for the AJAX 
-  * cross-domain request issue, during development.
-  * No sanitization of input is made either, so use this only
-  * if you are sure your requests are made correctly and
-  * your urls are valid.
-  *
-  */
-  $method = $_SERVER['REQUEST_METHOD'];
-  if ($_GET && $_GET['url']) {
-    $headers = getallheaders();
-    $headers_str = [];
-    $url = $_GET['url'];
-    
-    foreach ( $headers as $key => $value){
-      if($key == 'Host')
-        continue;
-      $headers_str[]=$key.":".$value;
-    }
-    $ch = curl_init($url);
-    curl_setopt($ch,CURLOPT_URL, $url);
-    if( $method !== 'GET') {
-      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-    }
-    if($method == "PUT" || $method == "PATCH" || ($method == "POST" && empty($_FILES))) {
-      $data_str = file_get_contents('php://input');
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $data_str);
-      //error_log($method.': '.$data_str.serialize($_POST).'\n',3, 'err.log');
-    }
-    elseif($method == "POST") {
-      $data_str = array();
-      if(!empty($_FILES)) {
-        foreach ($_FILES as $key => $value) {
-          $full_path = realpath( $_FILES[$key]['tmp_name']);
-          $data_str[$key] = '@'.$full_path;
-        }
-      }
-      //error_log($method.': '.serialize($data_str+$_POST).'\n',3, 'err.log');
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $data_str+$_POST);
-    }
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers_str );
-    $result = curl_exec($ch);
-    curl_close($ch);
-    header('Content-Type: application/json');
-    echo $result;
-  }
-  else {
-    echo $method;
-    var_dump($_POST);
-    var_dump($_GET);
-    $data_str = file_get_contents('php://input');
-    echo $data_str;
-  }
+<?php
+namespace Proxy;
+use Proxy\Event\ProxyEvent;
+use Proxy\Http\Request;
+use Proxy\Http\Response;
+use Proxy\Config;
+class Proxy {
+	
+	// Proxy script version
+	const VERSION = '5.1.0';
+	
+	private $dispatcher;
+	
+	private $request;
+	private $response;
+	
+	private $output_buffering = true;
+	private $output_buffer = '';
+	
+	private $status_found = false;
+	
+	public function __construct(){
+		// do nothing for now
+	}
+	
+	public function setOutputBuffering($output_buffering){
+		$this->output_buffering = $output_buffering;
+	}
+	
+	private function header_callback($ch, $headers){
+		$parts = explode(":", $headers, 2);
+		
+		// extract status code
+		// if using proxy - we ignore this header: HTTP/1.1 200 Connection established
+		if(preg_match('/HTTP\/1.\d+ (\d+)/', $headers, $matches) && stripos($headers, '200 Connection established') === false){
+			
+			$this->response->setStatusCode($matches[1]);
+			$this->status_found = true;
+		
+		} else if(count($parts) == 2){
+			
+			$name = strtolower($parts[0]);
+			$value = trim($parts[1]);
+			
+			// this must be a header: value line
+			$this->response->headers->set($name, $value, false);
+			
+		} else if($this->status_found){
+		
+			// this is hacky but until anyone comes up with a better way...
+			$event = new ProxyEvent(array('request' => $this->request, 'response' => $this->response, 'proxy' => &$this));
+			
+			// this is the end of headers - last line is always empty - notify the dispatcher about this
+			$this->dispatch('request.sent', $event);
+		}
+		
+		return strlen($headers);
+	}
+	
+	private function write_callback($ch, $str){
+	
+		$len = strlen($str);
+		
+		$this->dispatch('curl.callback.write', new ProxyEvent(array(
+			'request' => $this->request,
+			'data' => $str
+		)));
+		
+		// Do we buffer this piece of data for later output or not?
+		if($this->output_buffering){
+			$this->output_buffer .= $str;
+		}
+		
+		return $len;
+	}
+	
+	// TODO: move this all into its own Dispatcher class?
+	// https://github.com/guzzle/guzzle/blob/5.3/src/Event/Emitter.php
+	// https://github.com/laravel/framework/blob/5.0/src/Illuminate/Events/Dispatcher.php#L72
+	private $listeners = array();
+	
+	// Proxy instance itself acts as a dispatcher!
+	public function getEventDispatcher(){
+		return $this;
+	}
+	
+	public function addListener($event, $callback, $priority = 0){
+		$this->listeners[$event][$priority][] = $callback;
+	}
+	
+	public function addSubscriber($subscriber){
+		if(method_exists($subscriber, 'subscribe')){
+			$subscriber->subscribe($this);
+		}
+	}
+	
+	private function dispatch($event_name, $event){
+		
+		if(isset($this->listeners[$event_name])){
+			$temp = (array)$this->listeners[$event_name];
+			
+			foreach($temp as $priority => $listeners){
+				foreach( (array)$listeners as $listener){
+					if(is_callable($listener) ){
+						$listener($event);
+					}
+				}
+			}
+		}
+	}
+	
+	public function forward(Request $request, $url){
+	
+		// change request URL
+		$request->setUrl($url);
+		
+		// prepare request and response objects
+		$this->request = $request;
+		$this->response = new Response();
+		
+		$options = array(
+			CURLOPT_CONNECTTIMEOUT 	=> 10,
+			CURLOPT_TIMEOUT			=> 0,
+			
+			// don't return anything - we have other functions for that
+			CURLOPT_RETURNTRANSFER	=> false,
+			CURLOPT_HEADER			=> false,
+			
+			// don't bother with ssl
+			CURLOPT_SSL_VERIFYPEER	=> false,
+			CURLOPT_SSL_VERIFYHOST	=> false,
+			
+			// we will take care of redirects
+			CURLOPT_FOLLOWLOCATION	=> false,
+			CURLOPT_AUTOREFERER		=> false
+		);
+		
+		// this is probably a good place to add custom curl options that way other critical options below would overwrite that
+		$config_options = Config::get('curl', array());
+		
+		$options = array_merge_custom($options, $config_options);
+		
+		$options[CURLOPT_HEADERFUNCTION] = array($this, 'header_callback');
+		$options[CURLOPT_WRITEFUNCTION] = array($this, 'write_callback');
+		
+		// Notify any listeners that the request is ready to be sent, and this is your last chance to make any modifications.
+		$this->dispatch('request.before_send', new ProxyEvent(array(
+			'request' => $this->request,
+			'response' => $this->response
+		)));
+		
+		// We may not even need to send this request if response is already available somewhere (CachePlugin)
+		if($this->request->params->has('request.complete')){
+			// do nothing?
+		} else {
+		
+			// any plugin might have changed our URL by this point
+			$options[CURLOPT_URL] = $this->request->getUri();
+			
+			// fill in the rest of cURL options
+			$options[CURLOPT_HTTPHEADER] = explode("\r\n", $this->request->getRawHeaders());
+			$options[CURLOPT_CUSTOMREQUEST] = $this->request->getMethod();
+			$options[CURLOPT_POSTFIELDS] =  $this->request->getRawBody();
+			
+			$ch = curl_init();
+			curl_setopt_array($ch, $options);
+			
+			// fetch the status - if exception if throw any at callbacks, then the error will be supressed
+			$result = @curl_exec($ch);
+			
+			// there must have been an error if at this point
+			if(!$result){
+				$error = sprintf('(%d) %s', curl_errno($ch), curl_error($ch));
+				throw new \Exception($error);
+			}
+			
+			// we have output waiting in the buffer?
+			$this->response->setContent($this->output_buffer);
+			
+			// saves memory I would assume?
+			$this->output_buffer = null;
+		}
+		
+		$this->dispatch('request.complete', new ProxyEvent(array(
+			'request' => $this->request,
+			'response' => $this->response
+		)));
+		
+		return $this->response;
+	}
+}
+?>
